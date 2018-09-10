90d6133676662c07a9d8c3a294be18e1cb0a4344
Mickael <Mickael@MIG.home>
2018-09-10 08:34:51 +0200

    Modification de l'authentification
    Modification de la classe CustomAuthenticationManager pour corriger un
    défaut lors de la création de l'user/utilisateur

--- src/main/java/com/itescia/itbadge/security/CustomAuthenticationManager.java
+++ src/main/java/com/itescia/itbadge/security/CustomAuthenticationManager.java
@@ -1,5 +1,6 @@
 package com.itescia.itbadge.security;
 
+import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -7,6 +8,10 @@ import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
 
+import javax.naming.NamingException;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +24,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.ldap.authentication.BindAuthenticator;
 import org.springframework.security.ldap.authentication.LdapAuthenticationProvider;
 import org.springframework.security.ldap.search.FilterBasedLdapUserSearch;
@@ -27,11 +33,20 @@ import org.springframework.stereotype.Component;
 
 import com.itescia.itbadge.domain.Authority;
 import com.itescia.itbadge.domain.User;
+import com.itescia.itbadge.domain.Utilisateur;
+import com.itescia.itbadge.repository.CoursRepository;
+import com.itescia.itbadge.repository.GroupeRepository;
 import com.itescia.itbadge.repository.UserRepository;
+import com.itescia.itbadge.repository.UtilisateurRepository;
 import com.itescia.itbadge.service.MailService;
+import com.itescia.itbadge.service.UploadService;
 import com.itescia.itbadge.service.UserService;
+import com.itescia.itbadge.service.UtilisateurService;
 import com.itescia.itbadge.service.dto.UserDTO;
+import com.itescia.itbadge.service.impl.UtilisateurServiceImpl;
+import com.itescia.itbadge.service.util.RandomUtil;
 import com.itescia.itbadge.web.rest.UserResource;
+import com.itescia.itbadge.web.rest.UtilisateurResource;
 
 
 @Component
@@ -44,6 +59,13 @@ public class CustomAuthenticationManager implements AuthenticationManager {
 
     @Autowired
     private UserService userService;
+    
+    @Autowired
+    private UtilisateurRepository utilisateurRepository;
+    @Autowired
+    private CoursRepository coursRepository;
+    @Autowired
+    private GroupeRepository groupeRepository;
 
     LdapAuthenticationProvider provider = null;
 
@@ -75,6 +97,7 @@ public class CustomAuthenticationManager implements AuthenticationManager {
             java.util.logging.Logger.getLogger(CustomAuthenticationManager.class.getName()).log(null);
         }
         provider = new LdapAuthenticationProvider(bindAuth);
+		
         provider.setUserDetailsContextMapper(new UserDetailsContextMapper() {
             @Override
             public UserDetails mapUserFromContext(DirContextOperations ctx, String username, Collection<? extends GrantedAuthority> clctn) {
@@ -84,23 +107,57 @@ public class CustomAuthenticationManager implements AuthenticationManager {
                 if (!isUser.isPresent())
                 {
                 	try {
-                		UserDTO uDTO = new UserDTO();
-                		uDTO.setLogin(authentication.getName());
-                		uDTO.setActivated(true);
-                		uDTO.setFirstName(authentication.getName());
-                		uDTO.setLastName(authentication.getName());
-                		uDTO.setEmail(authentication.getName()+"@edu.itescia.fr");
-                		Set<String> gA = new HashSet<String>();
-                		gA.add("ROLE_USER");
-                		uDTO.setAuthorities(gA);
                 		
-						new UserResource(userService, userRepository, mailService).createUser(uDTO);
-						isUser = userRepository.findOneByLogin(username);
-					} catch (URISyntaxException e) {
+                		UtilisateurService uS = new UtilisateurServiceImpl(utilisateurRepository, coursRepository, groupeRepository);
+    		            Utilisateur util = new Utilisateur();
+    		            User user = new User();
+    		            
+    		            
+    		            user.setLastName(authentication.getName());
+    		            user.setFirstName(authentication.getName());
+    		            user.setLogin(authentication.getName());
+    		            user.setEmail(authentication.getName() + "@edu.itescia.fr");
+    		            
+    		            //Production setting
+//    		            user.setLastName(userSearch.searchForUser(authentication.getName()).getAttributes().get("nom").get().toString());
+//    		            user.setFirstName(userSearch.searchForUser(authentication.getName()).getAttributes().get("prenom").get().toString());
+//    		            user.setLogin(userSearch.searchForUser(authentication.getName()).getAttributes().get("uid").get().toString());
+//    		            user.setEmail(userSearch.searchForUser(authentication.getName()).getAttributes().get("mail").get().toString());
+    		            user.setActivated(true);
+    		            String encryptedPassword = new BCryptPasswordEncoder().encode(RandomUtil.generatePassword());;
+    		            user.setPassword(encryptedPassword);
+    		            Set<Authority> gA = new HashSet<Authority>();
+    		            Authority a = new Authority();
+    		            a.setName("ROLE_USER");
+    		            gA.add(a);
+                		user.setAuthorities(gA);					
+    		            util.setUser(user);
+    		            
+    		            if (userSearch.searchForUser(authentication.getName()).getDn().toString().contains("ou=admins"))
+    		            {
+    		            	util.setIsAdmin(true);
+        		            util.setIsProfesseur(false);
+    		            }
+    		            else if (userSearch.searchForUser(authentication.getName()).getDn().toString().contains("ou=professeurs"))
+    		            {
+    		            	util.setIsAdmin(false);
+        		            util.setIsProfesseur(true);
+    		            }
+    		            else
+    		            {
+    		            	util.setIsAdmin(false);
+        		            util.setIsProfesseur(false);
+    		            }
+    		            
+    		            
+    		            uS.save(util);
+    		            isUser = userRepository.findOneByLogin(username);
+    		            
+    					} catch (Exception e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
-                	
+                		
                 }
                 
                 final User user = isUser.get();
@@ -112,6 +169,7 @@ public class CustomAuthenticationManager implements AuthenticationManager {
                             a.getName());
                     grantedAuthorities.add(grantedAuthority);
                 }
+ 
                   return new org.springframework.security.core.userdetails.User(
                     username, "1" , grantedAuthorities);    
             }
